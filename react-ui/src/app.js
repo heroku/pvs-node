6471b3ca7d924a7de1cbc6c1644e6fbb45163968
Mars Hall <mars@heroku.com>
2016-11-13 15:17:21 -0800

    Barebones image uploader in React.

--- react-ui/src/app.js
+++ react-ui/src/app.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Dropzone from 'react-dropzone';
+import superagent from 'superagent';
 import './app.css';
 
 class App extends Component {
+
+  state = {
+    files: [],
+    uploadResponse: null
+  }
+
   render() {
+    const file = this.state.files[0];
+    const uploadResponse = this.state.uploadResponse;
+    
     return (
-      <div className="app">
-        <div className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="app-intro">
-          To get started, edit <code>src/app.js</code> and save to reload.
-        </p>
+      <div className="app">    
+
+        <Dropzone
+          accept={'image/png, image/jpeg'}
+          multiple={false}
+          onDrop={this.onDrop}>
+          <div>Drop image here or tap to upload</div>
+        </Dropzone>
+
+        {file != null
+          ? <img
+              alt="file-preview"
+              src={file.preview}/>
+          : null}
+
+        {uploadResponse != null
+          ? <pre>
+              {JSON.stringify(uploadResponse, null, 2)}
+            </pre>
+          : null}
       </div>
     );
+  }
+
+  onDrop = (acceptedFiles, rejectedFiles) => {
+    console.log('onDrop this: ', this);
+    console.log('Accepted files: ', acceptedFiles);
+    console.log('Rejected files: ', rejectedFiles);
+    this.setState({
+      files: acceptedFiles
+    });
+
+    var req = superagent.post('/file-upload');
+    acceptedFiles.forEach((file)=> {
+      // Backend expects 'file' reference
+      req.attach('file', file, file.name);
+    });
+    req.end((err,res) => {  
+      console.log('file-upload error', err);
+      console.log('file-upload response', res);
+      this.setState({
+        uploadResponse: JSON.parse(res.body)
+      });
+    });
+
   }
 }
 
